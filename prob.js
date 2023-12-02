2d580e20acf367dd7680b5734e7b305e97589abe
6394981696 <121605873+6394981696@users.noreply.github.com>
2023-12-02 13:45:21 +0530

    Update prob.js

--- prob.js
+++ prob.js
@@ -16,9 +16,9 @@ function CreateAuthor(name, birthYear, nationality) {
   };
   
   let author1 = new CreateAuthor("The India Story'", 1999, "Bimal Jalal");
-  let book1 = new CreateBook("Harry Potter and the Sorcerer's Stone", author1, "Fantasy", 15.99);
+  let book1 = new CreateBook("India Story", author1, "Story", 33.99);
   let author2 = new CreateAuthor("'A Place Called Home'", 1964, "Preeti Shenoy");
-  let book2 = new CreateBook("The Da Vinci Code", author2, "Mystery", 12.99);
+  let book2 = new CreateBook("The Da Vinci Code", author2, "Mystery", 32.99);
   
   book1.getBookInfo();
   book2.getBookInfo();
